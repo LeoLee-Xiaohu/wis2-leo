2ba427369b0e3f9e6ac15d030d61e5058631753c
Leo Li <xiaohu.li@utas.edu.au>
2025-09-04 11:49:56 +1000

    Refactor project structure: remove old ETL files, update package metadata, and add new example package with basic functionality

---
+++ packaging_tutorial/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from packaging-tutorial!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ test.py
@@ -0,0 +1,4 @@
+from wis2_aodn_upstream_flow.etl.load import load_to_minio
+
+
+print(load_to_minio)  # noqa: T201
